f1c70fd19d0c8c9f2d2a001ad69da14874e82856
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-09-08 20:00:08 +0300

    Support QuickCheck-2.9.1

--- test/Orphans.hs
+++ test/Orphans.hs
@@ -5,7 +5,6 @@ module Orphans where
 import Prelude                   ()
 import Prelude.Compat
 
-import Data.Version              (Version (..))
 import Test.Tasty.QuickCheck
 
 import Data.Orphans              ()
@@ -15,16 +14,18 @@ import Test.QuickCheck.Instances ()
 import Data.Vector               as V
 #endif
 
-#if !MIN_VERSION_QuickCheck(2,8,3)
-import Control.Applicative       (Const (..))
-import Data.List.NonEmpty        (NonEmpty (..))
-import Data.Proxy                (Proxy (..))
-import Data.Tagged               (Tagged (..))
-#endif
-
-#if !(MIN_VERSION_QuickCheck(2,8,0) && MIN_VERSION_base(4,8,0))
+#if !(MIN_VERSION_QuickCheck(2,8,0) && MIN_VERSION_base(4,8,0)) && !(MIN_VERSION_QuickCheck(2,9,0))
 import Numeric.Natural           (Natural)
 #endif
+
+#if !MIN_VERSION_QuickCheck(2,9,0)
+import Control.Applicative       (Const (..))
+import Data.List.NonEmpty        (NonEmpty (..))
+import Data.Version              (Version (..))
+#endif
+
+import Data.Proxy                (Proxy (..))
+import Data.Tagged               (Tagged (..))
 
 #if !MIN_VERSION_quickcheck_instances(0,3,12)
 instance Arbitrary a => Arbitrary (Vector a) where
@@ -32,30 +33,30 @@ instance Arbitrary a => Arbitrary (Vector a) where
   shrink    =  fmap V.fromList . shrink . V.toList
 #endif
 
-#if !(MIN_VERSION_QuickCheck(2,8,0) && MIN_VERSION_base(4,8,0))
+#if !(MIN_VERSION_QuickCheck(2,8,0) && MIN_VERSION_base(4,8,0)) && !(MIN_VERSION_QuickCheck(2,9,0))
 instance Arbitrary Natural where
   arbitrary = fmap (fromInteger . abs) arbitrary
 #endif
 
-instance Arbitrary Version where
-  arbitrary = do
-    x <- fmap abs arbitrary
-    xs <- (fmap . fmap) abs arbitrary
-    return $ Version (x : xs) []
-
-#if !MIN_VERSION_QuickCheck(2,8,3)
+#if !MIN_VERSION_QuickCheck(2,9,0)
 instance Arbitrary a => Arbitrary (NonEmpty a) where
   arbitrary = do
     x <- arbitrary
     xs <- arbitrary
     return (x :| xs)
 
-instance Arbitrary a => Arbitrary (Tagged t a) where
-  arbitrary = fmap Tagged arbitrary
-
 instance Arbitrary a => Arbitrary (Const a b) where
   arbitrary = fmap Const arbitrary
 
+instance Arbitrary Version where
+  arbitrary = do
+    x <- fmap abs arbitrary
+    xs <- (fmap . fmap) abs arbitrary
+    return $ Version (x : xs) []
+#endif
+
+instance Arbitrary a => Arbitrary (Tagged t a) where
+  arbitrary = fmap Tagged arbitrary
+
 instance Arbitrary (Proxy a) where
   arbitrary = return Proxy
-#endif
